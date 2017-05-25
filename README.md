220b999ab8d3292dcb31317f5800ed0d8569e737
Robert Olsson <robert@herjulf.se>
2017-05-25 20:15:05 +0200

    modified:   README.md 	modified:   mics2714.c

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Transfer functions for Nitrogen sensor
 
 
 hello-sensors.c Contiki test replaces hello-sensors for the avr-rss2 platform
+
+
+https://uk-air.defra.gov.uk/assets/documents/reports/cat06/0502160851_Conversion_Factors_Between_ppb_and.pdf
+
+
+On the UK Air Quality Archive, and for reporting data to the European 
+Commission, the appropriate conversion factors at 20C and 1013 mb are used. 
+
+EC  20C 1013mB  NO2 1 ppb= 1.9125 μg/m**3 
+WHO 25C 1013mB  NO2 1 ppb= 1.88   μg/m**3 
--- mics2714.c
+++ mics2714.c
@@ -2,17 +2,48 @@
 #include <math.h>
 /* 2017-04-27 -ro */
 
+#define MIC2714_M  0.9986
+#define MIC2714_A  0.163
+double m = MIC2714_M;
+double a = MIC2714_A;
+
+/*
+  EC  20C 1013mB  NO2 1 ppb= 1.9125 μg/m**3 
+  WHO 25C 1013mB  NO2 1 ppb= 1.88   μg/m**3 
+
+  https://uk-air.defra.gov.uk/assets/documents/reports/cat06/0502160851_Conversion_Factors_Between_ppb_and.pdf
+
+*/
+
+#define NO2_CONV_EC  1.9125
+#define NO2_CONV_WHO 1.88
+
+double mics2714(double vcc, double v0, double corr)
+{
+  double no2, rsr0;
+  /* Voltage divider */
+  rsr0 = (vcc - v0)/v0;
+  /* Transfer function */
+  no2 = a * pow(rsr0, m);
+  return no2 + corr;
+}
+
 main()
 {
-  double a, m, no2, rsr0;
+  double v0, no2, rsr0, vcc = 5;
+
+  for(v0 = 0.1; v0 <= 4.6; v0 += 0.5)  { 
+    no2 = mics2714(vcc, v0, 0);
+    printf(" v0=%4.2f NO2=%4.2f ppm\n", v0, no2);
+  }
+}
+
+void print_function(void)
+{  
+  double no2, rsr0;
   
-  m = 0.9986;
-  a = 0.163;
-
-
   for(rsr0=0.1; rsr0 <= 15; rsr0 = rsr0*1.2)  { 
     no2 = a * pow(rsr0, m);
     printf(" RS/R0=%4.2f NO2=%4.2f ppm\n", rsr0, no2);
   }
-
 }
